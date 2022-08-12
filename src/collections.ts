709703f9dd7fec38831d14a4c2ef23d7b9e26bba
mshanemc <shane.mclaughlin@salesforce.com>
2022-08-12 12:07:33 -0500

    feat: ensureArray

---
+++ src/collections.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * Normalize an object to be an array if it isn't one.
+ *
+ * @param entryOrArray - An object that could be an array of its type or just its type
+ * @returns An array of the input element (which might be empty)
+ */
+
+export const ensureArray = <T>(entryOrArray: T | T[] | undefined): T[] => {
+  if (entryOrArray) {
+    return Array.isArray(entryOrArray) ? entryOrArray : [entryOrArray];
+  }
+  return [];
+};
