c2f3b488a0fa636fb701282fb1ace46763a74c93
mshanemc <shane.mclaughlin@salesforce.com>
2024-03-21 10:51:52 -0500

    chore: jsdoc explanation

--- src/log.ts
+++ src/log.ts
@@ -5,8 +5,19 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+/**
+ * A wrapper for console.log that returns the input value unmodified
+ *
+ * ``` ts
+ * // peek inside a chain of map functions
+ * [1,2,3,4,5].map(logFn).map(yourNextFunction)
+ *
+ * // wrap a returned function call to see what it returns
+ * return logFn(otherFunction())
+ * ```
+ */
 export const logFn = <T>(x: T): T => {
   // eslint-disable-next-line no-console
-  console.log(typeof x === 'string' ? x : JSON.stringify(x, null, 2));
+  console.log(typeof x === 'object' ? JSON.stringify(x, null, 2) : x);
   return x;
 };
