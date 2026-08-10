a00337b073d9a31df20a0aa884561e9bc93a9e9a
Willie Ruemmele <willieruemmele@gmail.com>
2026-08-10 14:33:21 -0600

    chore: updates from devScripts

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-// Generated - Do not modify. Controlled by @salesforce/dev-scripts
-// See more at https://github.com/forcedotcom/sfdx-dev-packages/tree/master/packages/dev-scripts
-
-module.exports = {
-  extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license'],
-  rules: {
-    '@typescript-eslint/ban-types': 'off',
-  },
-};
